a1161450adca2e23dc19616265f5bc1d72e7b8c1
Levent GUZELOGLU <gun.guzeloglu@gmail.com>
2018-09-05 13:19:41 +0300

    Part5. Save data to mlab using /book method

--- Library/books/books.js
+++ Library/books/books.js
@@ -1,14 +1,19 @@
 //Load express
 const express = require("express");
 const app = express();
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
+//app.use(bodyParser.urlencoded({ extended: false }));//if you get error gettting req.body open comment
 //configure bodyParser.
 app.use(bodyParser.json());
 //Load mongoosee
 const mongoose = require("mongoose");
+//import Book model
+require("./Book");
+const Book = mongoose.model("Book");
+
 //Connect
 mongoose.connect(
-  "mongodb://xxx:xxx@ds245022.mlab.com:45022/booksservice",
+  "mongodb://XXX:XXX@ds245022.mlab.com:45022/booksservice",
   () => {
     console.log("DB Connected!");
   }
@@ -19,10 +24,30 @@ app.get("/", (req, res) => {
 });
 
 //Create func
-app.post("/book",(req,res)=>{
-//This is our create func
-  console.log(req.body);
-  res.send("Testing our book route!");
+app.post("/book", (req, res) => {
+  //This is our create func
+  console.log(req.body); //For Testing Purposes
+
+  //res.send("Testing our book route!"); //For Testing Purposes
+  var newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    numberPages: req.body.numberPages,
+    publisher: req.body.publisher
+  };
+  //Create a new Book with above req parameters
+  var book = new Book(newBook);
+  book
+    .save()
+    .then(() => {
+      console.log("New book created!");
+    })
+    .catch(err => {
+      if (err) {
+        throw err;
+      }
+    });
+  res.send("A new book is created with success!");
 });
 
 //Open express server
